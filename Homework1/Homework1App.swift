4cec338d1758cfe2c745c02b7eb8ade3542d162a
JaekeeAI <longtrinh333@gmail.com>
2024-01-15 22:40:12 -0800

    Initial Commit

---
+++ Homework1/Homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework1App.swift
+//  Homework1
+//
+//  Created by Jackie Trinh on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
